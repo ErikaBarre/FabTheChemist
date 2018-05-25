65c864934a912973c56308f9802ef04773cb4429
fabrice <fabrice@schopenhauer>
2018-05-25 23:34:20 +0200

    target

---
+++ FabTheChemistSqlDbSpring/src/main/java/fab/chemist/db/spring/db/framework/repository/interf/test.java
@@ -0,0 +1,13 @@
+package fab.chemist.db.spring.db.framework.repository.interf;
+
+import org.springframework.data.repository.CrudRepository;
+
+public class test {
+	
+	public void fqfd() {
+		
+		
+		CrudRepository c = null; 
+	}
+
+}
